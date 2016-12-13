0ba75fc9171d1420d4412536659026fc116cd8bd
Calvert <yz3210@gmail.com>
2016-12-13 10:03:12 +0800

    Add devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create!(
+  email: 'user1@localhost.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+user2 = User.create!(
+  email: 'user2@localhost.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
